838247426c7cb6cc7b9b7ca6f42f1279515f0fd5
MarceloPrgJr <josemarceloisidorio@gmail.com>
2020-04-11 12:03:34 -0300

    Modificações extras

--- index.c
+++ index.c
@@ -40,34 +40,35 @@ int main() {
     getchar(); //Limpa o Buffer
 
     switch (Global.Op) {
-    case 1:
-        Cadastro(); //Funcão Cadastro é iniciada
-        break;
+        case 1:
+            Cadastro(); //Funcão Cadastro é iniciada
+            break;
     
-    case 2:
-        printf("Função Alterar Iniciada...\n");
-        break;
+        case 2:
+            printf("Função Alterar Iniciada...\n");
+            break;
 
-    case 3:
-        Listar(); //Função Listar é iniciada
-        break;
+        case 3:
+            Listar(); //Função Listar é iniciada
+            break;
 
-    case 4:
-        printf("Função Remover Iniciada...\n");
-        break;
+        case 4:
+            printf("\nFunção Remover Iniciada...\n");
+            break;
 
-    case 5:
-        system("clear");
-        printf("######################\n");
-        printf("# Fim da Execução... #\n");
-        printf("######################\n\n");
-        break;
+        case 5:
+            system("clear");
+            printf("######################\n");
+            printf("# Fim da Execução... #\n");
+            printf("######################\n\n");
+            break;
     
-    default:
-        printf("Valor inválido! Tente novamente...\n");
-        sleep(1); //Pausa a tela por 1s (Linux)
-        main();
-        break;
+        default:
+            printf("Valor inválido! Tente novamente...\n");
+            sleep(1); //Pausa a tela por 1s (Linux)
+            main();
+            break;
+
     }
 
     return 0;
@@ -77,55 +78,56 @@ void Cadastro() {
     system("clear");
     DB = fopen("dados.txt", "a"); //Abertura do DB para a inserção de dados
 
-    if(DB == NULL) {
+    if(DB == NULL) { //Verificação do Banco de Dados
         printf("\nErro no Banco de Dados...\n");
         return 1;
 
-    } //Verificação do Banco de Dados
-
-    printf("==========\n");
-    printf(" CADASTRO\n");
-    printf("==========\n");
-
-    printf("\nDigite seu nome: ");
-    fgets(Pessoa.Nome, 25, stdin); //Recebe a String Nome
-    printf("\nDigite seu E-mail: ");
-    fgets(Pessoa.Email, 35, stdin); //Recebe a String Email
-    printf("\nDigite seu CPF: ");
-    fgets(Pessoa.Cpf, 15, stdin); //Recebe a String Cpf
-
-    system("clear");
-    printf("\n=============================\n");
-    printf("Finalizar cadastro? [s/n] : ");
-    scanf("%c", &Global.Cd); //Conclusão do cadastro
-
-    if(Global.Cd == 's') {
-    fprintf(DB, "Nome: %sE-mail: %sCPF: %s", Pessoa.Nome, Pessoa.Email, Pessoa.Cpf);
-    fprintf(DB, "----------------------\n"); //Salva os dados
-
-    fclose(DB); //Fecha o Banco de dados
-
-    system("clear");
-    printf("######################\n");
-    printf("# Cadastro Concluído #\n");
-    printf("######################\n");
-
-    sleep(1);
-    main();
-
-    } else if(Global.Cd == 'n') {
-        system("clear");
-        printf("Cadastro Cancelado.\nVoltando ao Menu Principal...\n");
-        fclose(DB); //Fecha DB
-        getchar(); //Corrige bug quando Global.Cd != 's' && 'n'        
-        sleep(1);
-        main();
-
     } else {
-        printf("Valor inválido. Tente novamente...\n");
-        fclose(DB); //Fecha DB
-        sleep(1);
-        Cadastro();
+        printf("==========\n");
+        printf(" CADASTRO\n");
+        printf("==========\n");
+
+        printf("\nDigite seu nome: ");
+        fgets(Pessoa.Nome, 25, stdin); //Recebe a String Nome
+        printf("\nDigite seu E-mail: ");
+        fgets(Pessoa.Email, 35, stdin); //Recebe a String Email
+        printf("\nDigite seu CPF: ");
+        fgets(Pessoa.Cpf, 15, stdin); //Recebe a String Cpf
+
+        system("clear");
+        printf("\n=============================\n");
+        printf("Finalizar cadastro? [s/n] : ");
+        scanf("%c", &Global.Cd); //Conclusão do cadastro
+
+        if(Global.Cd == 's') {
+            fprintf(DB, "Nome: %sE-mail: %sCPF: %s", Pessoa.Nome, Pessoa.Email, Pessoa.Cpf);
+            fprintf(DB, "----------------------\n"); //Salva os dados
+
+            fclose(DB); //Fecha o Banco de dados
+
+            system("clear");
+            printf("######################\n");
+            printf("# Cadastro Concluído #\n");
+            printf("######################\n");
+
+            sleep(1);
+            main();
+
+        } else if(Global.Cd == 'n') {
+            system("clear");
+            printf("Cadastro Cancelado.\nVoltando ao Menu Principal...\n");
+            fclose(DB); //Fecha DB
+            getchar(); //Corrige bug quando Global.Cd != 's' && 'n'        
+            sleep(1);
+            main();
+
+        } else {
+            printf("Valor inválido. Tente novamente...\n");
+            fclose(DB); //Fecha DB
+            sleep(1);
+            Cadastro();
+
+        }
 
     }
 
@@ -135,36 +137,39 @@ void Listar() {
     system("clear");
     DB = fopen("dados.txt", "r"); //Abertura do DB para leitura de dados
 
-    if(DB == NULL) {
+    if(DB == NULL) { //Verificação do Banco de dados
         printf("\nErro no Banco de Dados...\n");
         return 1;
 
-    } //Verificação do Banco de Dados
+    } else {
+        printf("======================\n");
+        printf(" LISTAGEM DE CLIENTES\n");
+        printf("======================\n\n");
 
-    char listagem[100]; //String geral para os dados
+        char listagem[250]; //String geral para os dados
 
-    printf("======================\n");
-    printf(" LISTAGEM DE CLIENTES\n");
-    printf("======================\n\n");
+        while(fgets(listagem, 250, DB) != NULL) {
+            printf("%s", listagem);
 
-    while(fgets(listagem, 1000, DB) != NULL) {
-        printf("%s", listagem);
+        } //Considera todos os dados como uma String e exibe na tela
 
-    } //Considera todos os dados como uma String e exibe na tela
+        fclose(DB);
 
-    fclose(DB);
+        printf("\nVoltar ao Menu Principal? [s/n]: ");
+        scanf("%c", &Global.Cd);
 
-    printf("\nVoltar ao Menu Principal? [s/n]: ");
-    scanf("%c", &Global.Cd);
+        if(Global.Cd == 's') {
+            main();
 
-    if(Global.Cd == 's') {
-        main();
+        } else if(Global.Cd != 's' && Global.Cd != 'n') {
+            printf("Valor inválido.\nVoltando ao Menu Principal...\n");
+            sleep(1);
+            main();
 
-    } else if(Global.Cd != 's' && Global.Cd != 'n') {
-        printf("Valor inválido.\nVoltando ao Menu Principal...\n");
-        sleep(1);
-        main();
+        }
 
     }
 
-}
+
+
+} //Fim da função Listar
